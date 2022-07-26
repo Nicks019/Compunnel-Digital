c97d7aa2d3d9ca4dd9f5a61fe364c67d832416d6
tanmay-churi <98447290+tanmay-churi@users.noreply.github.com>
2022-07-27 02:16:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -80,6 +80,8 @@ scale_factor = 1/scaler[0]
 y_predicted = y_predicted * scale_factor
 y_test = y_test * scale_factor
 
+st.write('y_predicted')
+
 #Final Graph
 st.subheader('Prediction vs Original')
 fig2 = plt.figure(figsize=(12,6))
