20efa7b72ea87da0127be4d4b5e8a68dde6f385b
tanmay-churi <98447290+tanmay-churi@users.noreply.github.com>
2022-07-27 16:56:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -89,7 +89,9 @@ y_test = scaler.inverse_transform(y_test)
 yp = pd.DataFrame(y_predicted,columns=(['Predicted']))
 yt = pd.DataFrame(y_test,columns=(['Actual']))
 y = pd.concat([yt,yp],axis = 1)
-st.write(y)
+
+st.subheader('Actual Vs Predicted Prices')
+st.write(y.head(5))
 
 #Final Graph
 st.subheader('Prediction vs Original')
